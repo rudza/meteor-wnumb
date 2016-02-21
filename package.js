e23669eb97eabbbdb1915d20661b05249d9b5156
Rudolfs Janitis <rudolfs@codecraft.lv>
2016-02-21 19:08:26 +0200

    Last fixes

--- package.js
+++ package.js
@@ -8,14 +8,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('ecmascript');
   api.addFiles('wNumb.js');
   api.export('wNumb', ['client','server']);
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
   api.use('tinytest');
   api.use('rudolfs:wnumb');
-  api.addFiles('wnumb-tests.js');
 });
