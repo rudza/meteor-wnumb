62787b2669afe55dfc0ac9eb44ad764736944d2d
Rudolfs Janitis <rudolfs@codecraft.lv>
2016-02-21 18:55:43 +0200

    Exported wNumb to client and server

--- package.js
+++ package.js
@@ -1,19 +1,16 @@
 Package.describe({
   name: 'rudolfs:wnumb',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '0.0.2',
+  summary: 'wNumb.js for Meteor',
+  git: 'https://github.com/rudza/meteor-wnumb',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('wnumb.js');
+  api.addFiles('wNumb.js');
+  api.export('wNumb', ['client','server']);
 });
 
 Package.onTest(function(api) {
